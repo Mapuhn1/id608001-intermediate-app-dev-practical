e11eeb3ae94e688fdfa6c5bf78314fa7b3719432
Hazel <mapuhn1@student.op.ac.nz>
2025-09-15 09:30:02 +1200

    feat: set variable to store array of returned data

--- HackerNews/src/components/stories/API.tsx
+++ HackerNews/src/components/stories/API.tsx
@@ -1,4 +1,5 @@
 export const fetchStoriesByType = async (type: string) => {
   const response = await fetch(`https://hacker-news.firebaseio.com/v0/${type}stories.json`);
+  const storyIds = await response.json();
   
 };
