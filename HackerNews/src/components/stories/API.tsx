f18871a502343600d74f30d6365a962078485ea4
Hazel <mapuhn1@student.op.ac.nz>
2025-09-15 09:33:35 +1200

    feat: for each id return full stpry object

--- HackerNews/src/components/stories/API.tsx
+++ HackerNews/src/components/stories/API.tsx
@@ -3,4 +3,12 @@ export const fetchStoriesByType = async (type: string) => {
   const storyIds = await response.json();
   const limitedIds = storyIds.slice(0, 50);
 
+
+  const stories = await Promise.all(
+    limitedIds.map(id =>
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(res => res.json())
+    )
+  );
+
+  return stories;
 };
