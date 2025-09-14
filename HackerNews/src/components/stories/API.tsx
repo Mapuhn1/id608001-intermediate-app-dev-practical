328d24da3036000b900e9d24dfc7cd5d0730001a
Hazel <mapuhn1@student.op.ac.nz>
2025-09-15 09:30:43 +1200

    feat: use slice to return the first 50 story Ids

--- HackerNews/src/components/stories/API.tsx
+++ HackerNews/src/components/stories/API.tsx
@@ -1,5 +1,6 @@
 export const fetchStoriesByType = async (type: string) => {
   const response = await fetch(`https://hacker-news.firebaseio.com/v0/${type}stories.json`);
   const storyIds = await response.json();
-  
+  const limitedIds = storyIds.slice(0, 50);
+
 };
