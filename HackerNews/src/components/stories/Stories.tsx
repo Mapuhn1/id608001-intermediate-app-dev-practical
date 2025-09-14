55dedcd0f834cf7e8e1d906c921361f5e340927a
Hazel <mapuhn1@student.op.ac.nz>
2025-09-15 08:47:02 +1200

    feat: created function to fetch frot 50 top stories

--- HackerNews/src/components/stories/Stories.tsx
+++ HackerNews/src/components/stories/Stories.tsx
@@ -1,2 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import '../../App.css';
+
+const Stories = () => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["top-stories"],
+    queryFn: async () => {
+      const response = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json");
+      const storyIds = await response.json();
+      
+      const first50Ids = storyIds.slice(0, 50);
+      
+      const stories = await Promise.all(
+        first50Ids.map(id =>
+          fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(res => res.json())
+        )
+      );
+      
+      return stories;
+    },
+  });
+
+};
