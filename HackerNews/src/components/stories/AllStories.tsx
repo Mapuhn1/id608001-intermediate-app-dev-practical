83fc69787583157a3d6b8aa6fef24e4245cbdc07
Hazel <mapuhn1@student.op.ac.nz>
2025-09-15 09:48:30 +1200

    feat: loop over each story type defined in the array using .map function

--- HackerNews/src/components/stories/AllStories.tsx
+++ HackerNews/src/components/stories/AllStories.tsx
@@ -3,4 +3,11 @@ import Stories from './Stories';
 const storyTypes = ["top", "ask","best", "job", "new", "show", ];
 
 const AllStories = () => {
+  return (
+    <div className="all-stories">
+      {storyTypes.map((type) => (
+        <Stories key={type} storyType={type} />
+      ))}
+    </div>
+  );
 };
