40e42307f1b4cb6a6128e021f9a5cded925a7e66
Hazel <mapuhn1@student.op.ac.nz>
2025-09-18 08:33:28 +1200

    feat: import broser router and wrap it around app component

--- HackerNews/src/main.tsx
+++ HackerNews/src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-<StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-</StrictMode>,
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>,
 )
