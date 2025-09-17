72819e7772ad6e35cde6a68195f9134772ee8732
Hazel <mapuhn1@student.op.ac.nz>
2025-09-18 08:07:51 +1200

    refactor: set queryKey to the storytype

--- HackerNews/src/components/stories/Stories.tsx
+++ HackerNews/src/components/stories/Stories.tsx
@@ -8,21 +8,8 @@ const Stories = () => {
   const storyType = searchParams.get("type") || "top";
 
   const { isLoading, error, data } = useQuery({
-    queryKey: ["top-stories"],
-    queryFn: async () => {
-      const response = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json");
-      const storyIds = await response.json();
-      
-      const first50Ids = storyIds.slice(0, 50);
-      
-      const stories = await Promise.all(
-        first50Ids.map(id =>
-          fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(res => res.json())
-        )
-      );
-      
-      return stories;
-    },
+    queryKey: [storyType],
+    queryFn: async () => fetchStoriesByType(storyType),
   });
 
   if (isLoading) return <div>Loading...</div>;
