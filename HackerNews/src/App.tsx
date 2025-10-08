68195a66ca45bb2875d17aff68b27417d2f7e846
Hazel <mapuhn1@student.op.ac.nz>
2025-10-09 09:41:22 +1300

    feat: set min height to fill screen and use flexbox

--- HackerNews/src/App.tsx
+++ HackerNews/src/App.tsx
@@ -3,6 +3,8 @@ import { Outlet } from 'react-router';
 function App() {
 
   return (
+    <div className="min-h-screen bg-gray-50 flex justify-center">
+      <div className="w-full max-w-[1280px] px-8 text-center">
         <header className="bg-gradient-to-r from-pink-500 to-pink-200">
           <div className="px-4 sm:px-6 lg:px-8 py-6">
             <h1 className="text-3xl font-bold text-white">
@@ -10,6 +12,8 @@ function App() {
             </h1>
           </div>
         </header>
+      </div>
+    </div>
   );
 }
 
