647ac061ffb1825701767e0a5043afad1a508cf1
Hazel <mapuhn1@student.op.ac.nz>
2025-10-09 09:34:59 +1300

    feat: style header with gradient fom pink to light pink

--- HackerNews/src/App.tsx
+++ HackerNews/src/App.tsx
@@ -3,10 +3,14 @@ import { Outlet } from 'react-router';
 function App() {
 
   return (
-    <>
-      <h1>HackerNews Application</h1>
-    </>
-  )
+        <header className="bg-gradient-to-r from-pink-500 to-pink-200">
+          <div className="px-4 sm:px-6 lg:px-8 py-6">
+            <h1 className="text-3xl font-bold text-white">
+              HackerNews Application
+            </h1>
+          </div>
+        </header>
+  );
 }
 
 export default App
