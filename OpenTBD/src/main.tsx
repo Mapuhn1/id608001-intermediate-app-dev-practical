fae96520fe6aebf4d7a619841d0ec22fc98611c3
Hazel <mapuhn1@student.op.ac.nz>
2025-10-23 13:29:41 +1300

    feat: wrap main app route in browser router

--- OpenTBD/src/main.tsx
+++ OpenTBD/src/main.tsx
@@ -5,10 +5,15 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import App from './App.tsx';
 
 import './index.css'
-import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  </StrictMode>
+);
