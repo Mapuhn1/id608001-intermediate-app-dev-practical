01098c45497bbc67fa31d573e0bc6203d9469cc0
Hazel <mapuhn1@student.op.ac.nz>
2025-10-29 22:39:23 +1300

    feat: create useQuiz hook for safe and reusable context access

--- OpenTBD/src/context/QuizContext.tsx
+++ OpenTBD/src/context/QuizContext.tsx
@@ -39,3 +39,9 @@ export const QuizProvider = ({ children }) => {
     </QuizContext.Provider>
   );
 };
+
+export const useQuiz = () => {
+  const context = useContext(QuizContext);
+  if (!context) throw new Error('useQuiz must be used within QuizProvider');
+  return context;
+};
