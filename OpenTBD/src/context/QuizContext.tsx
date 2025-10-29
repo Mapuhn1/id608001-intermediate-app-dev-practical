c9df0d9266e37e8c028d073c396d14b7ee477ed9
Hazel <mapuhn1@student.op.ac.nz>
2025-10-29 22:21:08 +1300

    feat: setup quizProvider component with children parameter

--- OpenTBD/src/context/QuizContext.tsx
+++ OpenTBD/src/context/QuizContext.tsx
@@ -2,3 +2,7 @@ import { createContext, useContext, useState, useEffect } from 'react';
 import { storage } from '../utils/storage';
 
 const QuizContext = createContext(undefined);
+
+export const QuizProvider = ({ children }) => {
+    
+};
