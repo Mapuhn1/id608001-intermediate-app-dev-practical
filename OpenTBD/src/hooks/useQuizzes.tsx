9f91442666fcd07b58c3d67fa5aafce3c515588f
Hazel <mapuhn1@student.op.ac.nz>
2025-11-03 08:23:30 +1300

    feat: setup quiz state initial state as an empty array

--- OpenTBD/src/hooks/useQuizzes.tsx
+++ OpenTBD/src/hooks/useQuizzes.tsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from 'react';
 import { storage } from '../utils/storage';
 
-export const useQuizzes = () => {};
+export const useQuizzes = () => {
+  const [quizzes, setQuizzes] = useState([]);
+
+};
