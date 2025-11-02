43f8bf6f9a80e7d4c7a72c494fc0001439eed0a8
Hazel <mapuhn1@student.op.ac.nz>
2025-11-03 08:49:49 +1300

    feat: set quiz form initial values

--- OpenTBD/src/components/quiz/QuizForm.tsx
+++ OpenTBD/src/components/quiz/QuizForm.tsx
@@ -2,3 +2,12 @@ import { useState } from 'react';
 import { useCategories } from '../../hooks/useCategories';
 import { useQuiz } from '../../context/QuizContext';
 import { fetchQuiz } from '../API';
+  const [form, setForm] = useState({
+    amount: 10,
+    category: '',
+    difficulty: '',
+    type: '',
+    startDate: '',
+    endDate: ''
+  });
+
