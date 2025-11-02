445ac84cc3d97554203837f7f7d3b4be69513cb6
Hazel <mapuhn1@student.op.ac.nz>
2025-11-03 08:48:27 +1300

    chore: import fetchQuiz from API component

--- OpenTBD/src/components/quiz/QuizForm.tsx
+++ OpenTBD/src/components/quiz/QuizForm.tsx
@@ -1,3 +1,4 @@
 import { useState } from 'react';
 import { useCategories } from '../../hooks/useCategories';
 import { useQuiz } from '../../context/QuizContext';
+import { fetchQuiz } from '../API';
