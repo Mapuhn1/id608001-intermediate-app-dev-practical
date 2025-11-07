4c3513b41cab00cf0b3055d1da3050fcec035ece
Hazel <mapuhn1@student.op.ac.nz>
2025-11-07 19:08:58 +1300

    feat: import calender icon from lucide

--- OpenTBD/src/components/shared/QuizCard.tsx
+++ OpenTBD/src/components/shared/QuizCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardHeader, CardContent, CardFooter } from '../ui/card';
 import { Button } from '../ui/button';
 import { CategoryBadge } from './CategoryBadge';
+import { Calendar } from 'lucide-react' ;
 import { useQuiz } from '../../context/QuizContext';
 
 export const QuizCard = ({ quiz, status, onPlay }) => {
