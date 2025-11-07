f50ccf3542f8ab253bc795bdb68b8bec144bcdb3
Hazel <mapuhn1@student.op.ac.nz>
2025-11-07 18:24:23 +1300

    chore: import categoryBadge component

--- OpenTBD/src/components/shared/QuizCard.tsx
+++ OpenTBD/src/components/shared/QuizCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardHeader, CardContent, CardFooter } from '../ui/card';
 import { Button } from '../ui/button';
+import { CategoryBadge } from './CategoryBadge';
 
 export const QuizCard = ({ quiz, status, onPlay }) => {
 };
