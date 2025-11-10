10faa5a2ca34f22fd76ed5e3e55302e0a0973167
Hazel <mapuhn1@student.op.ac.nz>
2025-11-10 16:02:46 +1300

    feat: display message when there are no quizzes

--- OpenTBD/src/components/quiz/QuizList.tsx
+++ OpenTBD/src/components/quiz/QuizList.tsx
@@ -5,4 +5,11 @@ export const QuizList = ({ onPlayQuiz }) => {
   const { categorizeQuizzes } = useQuizzes();
   const { past, current, future } = categorizeQuizzes();
 
+  if (past.length === 0 && current.length === 0 && future.length === 0) {
+    return (
+      <div className="text-center p-8 text-gray-500">
+        No quizzes created yet.
+      </div>
+    );
+  }
 };
