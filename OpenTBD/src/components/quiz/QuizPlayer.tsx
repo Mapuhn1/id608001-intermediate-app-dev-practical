b5f0f6f8e9b85a5ee11c2cbdd70f8e1c0bc75823
Hazel <mapuhn1@student.op.ac.nz>
2025-11-13 08:16:22 +1300

    feat: setup quizPlayer component

--- OpenTBD/src/components/quiz/QuizPlayer.tsx
+++ OpenTBD/src/components/quiz/QuizPlayer.tsx
@@ -3,3 +3,6 @@ import { Card, CardHeader, CardContent, CardFooter } from '../ui/card';
 import { Button } from '../ui/button';
 import { Progress } from '../ui/progress';
 
+export const QuizPlayer = ({}) => {
+  
+};
