efaa9af4c95ecbc5f8414f7b612724a1805fde40
Hazel <mapuhn1@student.op.ac.nz>
2025-11-13 08:16:56 +1300

    feat : pass in quiz and onComplete as arguments

--- OpenTBD/src/components/quiz/QuizPlayer.tsx
+++ OpenTBD/src/components/quiz/QuizPlayer.tsx
@@ -3,6 +3,6 @@ import { Card, CardHeader, CardContent, CardFooter } from '../ui/card';
 import { Button } from '../ui/button';
 import { Progress } from '../ui/progress';
 
-export const QuizPlayer = ({}) => {
+export const QuizPlayer = ({ quiz, onComplete }) => {
   
 };
