09d9f7244c271256f971153351adb989e2dbb8bc
Hazel <mapuhn1@student.op.ac.nz>
2025-11-14 09:16:07 +1300

    feat: destructure favourites and toggle from useFavourites hook

--- HackerNews/src/components/leaders/Leaders.tsx
+++ HackerNews/src/components/leaders/Leaders.tsx
@@ -6,6 +6,7 @@ import { useFavourites } from "../stories/useFavourites";
 const Leaders = () => {
   const [loading, setLoading] = useState(true);
   const [leadersData, setLeadersData] = useState([]);
+  const { favourites, toggle } = useFavourites();
 };
 
 export default Leaders;
