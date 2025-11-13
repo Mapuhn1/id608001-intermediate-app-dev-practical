f2605c76bb90d4e59de5be65caa1e2dd9d1cf963
Hazel <mapuhn1@student.op.ac.nz>
2025-11-14 09:14:26 +1300

    feat: ssetup Leaders component and set initial loading and leadersdata state

--- HackerNews/src/components/leaders/Leaders.tsx
+++ HackerNews/src/components/leaders/Leaders.tsx
@@ -2,3 +2,7 @@ import { useState, useEffect } from "react";
 import { leaders } from "./leaders-data";
 import { useFavourites } from "../stories/useFavourites";
 
+const Leaders = () => {
+  const [loading, setLoading] = useState(true);
+  const [leadersData, setLeadersData] = useState([]);
+};
